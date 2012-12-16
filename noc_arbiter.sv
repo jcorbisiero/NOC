11b957f1c5c14366b0986aab96d7d68f0c56dac4
I E <nighthawk6389@yahoo.com>
2012-12-16 17:29:13 -0500

    Arbiter was looking at old value of each turn register

--- noc_arbiter.sv
+++ noc_arbiter.sv
@@ -2,11 +2,11 @@ module arbiter(
 	input clk,
 	input rst,
 	
-	output reg [4:0] N_turn_o,
-	output reg [4:0] S_turn_o,
-	output reg [4:0] E_turn_o,
-	output reg [4:0] W_turn_o,
-	output reg [4:0] L_turn_o
+	output logic [4:0] N_turn_o,
+	output logic [4:0] S_turn_o,
+	output logic [4:0] E_turn_o,
+	output logic [4:0] W_turn_o,
+	output logic [4:0] L_turn_o
 );
 
 reg [4:0] NTurn;
@@ -32,22 +32,26 @@ always_ff @(posedge clk) begin
 		LTurn <= 5'b10000;	
 	end else begin
 		NTurn <= NTurn >> 1;
-		if (NTurn == 5'b00000) NTurn <= 5'b010000;
+		if (NTurn >> 1== 5'b00000) NTurn <= 5'b010000;
+		
+		$display("STurn is before %b", STurn);
 
 		STurn <= STurn >> 1;
-		if (STurn == 5'b01000) STurn <= STurn >> 1;
-		else if (STurn == 5'b00000) STurn <= 5'b10000;
+		if (STurn >> 1== 5'b01000) STurn <= STurn >> 1;
+		else if (STurn >> 1== 5'b00000) STurn <= 5'b10000;
 
 		ETurn <= ETurn >> 1;
-		if (ETurn == 5'b00100) ETurn <= ETurn >> 1;
-		else if (ETurn == 5'b00000) ETurn <= 5'b10000;
+		if (ETurn >> 1== 5'b00100) ETurn <= ETurn >> 1;
+		else if (ETurn >> 1== 5'b00000) ETurn <= 5'b10000;
 
 		WTurn <= WTurn >> 1;
-		if (WTurn == 5'b00010) WTurn <= WTurn >> 1;
-		else if (WTurn == 5'b00000) WTurn <= 5'b10000;
+		if (WTurn >> 1== 5'b00010) WTurn <= WTurn >> 1;
+		else if (WTurn >> 1== 5'b00000) WTurn <= 5'b10000;
 
 		LTurn <= LTurn >> 1;
-		if (LTurn == 5'b00001) LTurn <= 5'b10000;
+		if (LTurn >> 1== 5'b00001) LTurn <= 5'b10000;
+		
+		$display("STurn is now %b. O is %b", STurn, S_turn_o);
 	end
 end
 
