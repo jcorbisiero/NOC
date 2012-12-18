78577ae005a993dbcc2cb893ce174c9d99865197
Joseph Corbisiero <jc3790@columbia.edu>
2012-12-18 18:04:26 -0500

    updated route logic to check for errors if it was a ports turn but the coordinate was not correct

--- noc_routeLogic.sv
+++ noc_routeLogic.sv
@@ -90,7 +90,12 @@ L_port_remove = 0;
 				N_credit_inc = 1;
 			end
 
-		end else begin
+		end else if(N_data_i[3:0] < YCOORD) begin 
+			$display("Dropping packet sent to North");
+			N_port_remove = 1;
+			N_credit_inc = 1;
+		end
+		else begin
 			if (N_data_i[7:4] > XCOORD) begin
 				$display("Attempting East %d ", N_data_i[3:0]);
 				if (!E_port_full && (E_turn == 5'b10000)) begin
@@ -109,7 +114,6 @@ L_port_remove = 0;
 					N_credit_inc = 1;
 				end
 			end else if (!L_port_full && (L_turn == 5'b10000)) begin
-					
 				L_port_select = 3'b000;
 				N_port_remove = 1;
 				L_port_enable = 1;
@@ -133,6 +137,19 @@ L_port_remove = 0;
 				N_port_enable = 1;
 				S_credit_inc = 1;
 			end
+		end 
+		/* Added this in for the situation where if you are at 1,1 and want to send to 4,4
+		 * We can only go North/South first, then East/West
+		 * Here S_data_i's ycoord will be 4 and YCOORD will be 1 so the above if fails	
+		 * However, in the big else block below this if...else, S_data_i's xcoord will
+		 * be greater than XCOORD and so will attempt to send East but this will violate
+		 * the direction we are sending a packet because we must send South first
+		 * So the data packet will be dropped
+		 */
+		else if (S_data_i[3:0] > YCOORD) begin
+			$display("Dropping packet sent to South");
+			S_port_remove = 1;
+			S_credit_inc = 1;
 		end else begin
 			if (S_data_i[7:4] > XCOORD) begin
 				if (!E_port_full && (E_turn == 5'b01000)) begin
@@ -153,7 +170,7 @@ L_port_remove = 0;
 				S_port_remove = 1;
 				L_port_enable = 1;
 				S_credit_inc = 1;
-			end
+			end 
 		end
 	end
 
@@ -173,10 +190,14 @@ L_port_remove = 0;
 				W_port_enable = 1;
 				E_credit_inc = 1;
 			end
-		end else if (!L_port_full && (L_turn == 5'b00100)) begin
+		end else if (!L_port_full && (L_turn == 5'b00100) && E_data_i[7:4] == XCOORD) begin
 			L_port_select = 010;
 			E_port_remove = 1;
 			L_port_enable = 1;
+			E_credit_inc = 1;
+		end else begin
+			$display("Dropping packet sent to East");
+			E_port_remove = 1;
 			E_credit_inc = 1;
 		end
 	end
@@ -197,10 +218,22 @@ L_port_remove = 0;
 				E_port_enable = 1;
 				W_credit_inc = 1;
 			end
-		end else if (!L_port_full && (L_turn == 5'b00010)) begin
+		end
+		/*
+		 * added necessary check to see that packet's coordinate is indeed local
+		 * needed this to make sure that if we receive from west and are attempting to send
+		 * out west that since that cannot happen, we don't want the packet to go to local
+		 * because it is local's turn. it should only go local if it's local's turn and the
+		 * coordinate of the packet is to go local. Done for other ports as well
+		 */
+		else if (!L_port_full && (L_turn == 5'b00010)  && W_data_i[7:4] == XCOORD) begin
 			L_port_select = 011;
 			W_port_remove = 1;
 			L_port_enable = 1;
+			W_credit_inc = 1;
+		end else begin
+			$display("Dropping packet sent to West");
+			W_port_remove = 1;
 			W_credit_inc = 1;
 		end
 	end
